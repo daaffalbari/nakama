1afde0e8f00763e81bbc5101262ee68c15e855ff
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-02-03 19:58:41 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '@/styles/homepage.css';
+import '@/styles/utilities.css';
+import '@/styles/sign-in.css';
+import '@/styles/sign-up.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
